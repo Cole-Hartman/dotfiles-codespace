b32d355d349b000666e0bdcc08f8af3eb7e8de67
Cole Hartman <cole.hartman@github.com>
2025-05-22 18:53:18 -0700

    disable

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
-#!/usr/bin/env bash
-set -e
-
-echo "[dotfiles] Setting up environment..."
-
-### 1. Install Neovim (v0.9.5)
-if ! command -v nvim &>/dev/null || [[ "$(nvim --version | head -n1)" != *"v0.9.5"* ]]; then
-  echo "[dotfiles] Installing Neovim v0.9.5..."
-  curl -LO https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
-  tar xzf nvim-linux64.tar.gz
-  sudo mv nvim-linux64 /opt/nvim
-  sudo ln -sfn /opt/nvim/bin/nvim /usr/local/bin/nvim
-  rm nvim-linux64.tar.gz
-else
-  echo "[dotfiles] Neovim already installed."
-fi
-
-### 2. Symlink Neovim config
-echo "[dotfiles] Linking Neovim config..."
-ln -sfn /workspaces/.codespaces/.persistedshare/dotfiles/.config/nvim ~/.config/
-
-echo "[dotfiles] Done!"
+# #!/usr/bin/env bash
+# set -e
+#
+# echo "Setting up environment..."
+#
+# # Install Neovim (v0.9.5)
+# if ! command -v nvim &>/dev/null || [[ "$(nvim --version | head -n1)" != *"v0.9.5"* ]]; then
+#   echo "[dotfiles] Installing Neovim v0.9.5..."
+#   curl -LO https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
+#   tar xzf nvim-linux64.tar.gz
+#   sudo mv nvim-linux64 /opt/nvim
+#   sudo ln -sfn /opt/nvim/bin/nvim /usr/local/bin/nvim
+#   rm nvim-linux64.tar.gz
+# else
+#   echo "Neovim already installed."
+# fi
+#
+# # Symlink Neovim config
+# echo "Linking Neovim config..."
+# ln -sfn /workspaces/.codespaces/.persistedshare/dotfiles/.config/nvim ~/.config/
+#
+# echo "Done!"
 
